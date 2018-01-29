7df5aac17954941df1da039bad49e3091c61a675
Mike Natale <mnatale@streamsystems.ca>
2018-01-29 16:18:01 -0500

    Save Table Information on blur

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import {
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 
+const _LOCAL_STORAGE_ITEM_NAME = 'ContactListData';
+
 class App extends Component {
     constructor() {
         super();
@@ -20,15 +22,17 @@ class App extends Component {
         };
 
         this.renderTableRows = this.renderTableRows.bind(this);
+        this.updateTableRow = this.updateTableRow.bind(this);
+        this.saveData = this.saveData.bind(this);
     }
 
     componentWillMount() {
         let scope = this;
 
-        let data = localStorage.getItem('ContactListData');
+        let data = localStorage.getItem(_LOCAL_STORAGE_ITEM_NAME);
         if (!data) {
             data = '[{"firstName":"Mike","lastName":"Natale","age":26}]';
-            localStorage.setItem('ContactListData', data);
+            localStorage.setItem(_LOCAL_STORAGE_ITEM_NAME, data);
         }
 
         scope.setState({
@@ -36,27 +40,45 @@ class App extends Component {
         });
     }
 
+    updateTableRow(e) {
+        const rowNumber = parseInt(e.target.id.split("_")[0], 10);
+        const field = e.target.id.split("_")[1];
+
+        let data = this.state.data;
+        data[rowNumber][field] = e.target.value;
+
+        this.saveData(data);
+    }
+
+    saveData(data) {
+        localStorage.setItem(_LOCAL_STORAGE_ITEM_NAME, JSON.stringify(data));
+        this.setState({ data });
+    }
+
     renderTableRows() {
         let index = 0;
         return this.state.data.map((contact) => {
             let ret = (
-                <TableRow>
-                    <TableRowColumn>
+                <TableRow key={`tableRow${index}`}>
+                    <TableRowColumn key={`tableRow${index}_0`}>
                         <TextField
                             id={`${index}_lastName`}
                             defaultValue={contact.lastName}
+                            onBlur={this.updateTableRow}
                         />
                     </TableRowColumn>
-                    <TableRowColumn>
+                    <TableRowColumn key={`tableRow${index}_1`}>
                         <TextField
                             id={`${index}_firstName`}
                             defaultValue={contact.firstName}
+                            onBlur={this.updateTableRow}
                         />
                     </TableRowColumn>
-                    <TableRowColumn>
+                    <TableRowColumn key={`tableRow${index}_2`}>
                         <TextField
                             id={`${index}_age`}
                             defaultValue={contact.age}
+                            onBlur={this.updateTableRow}
                         />
                     </TableRowColumn>
                 </TableRow>
